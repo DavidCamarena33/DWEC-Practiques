48b98d88d789ff7d4729cde1359a8a5aafaecee5
David <davidcugamer23@gmail.com>
2025-09-16 19:40:29 +0200

    peli constructor

---
+++ pel·licula.js
@@ -0,0 +1,8 @@
+class pel·licula{
+    constructor(titol, director, genere, disponibles){
+        this.titol = titol;
+        this.director = director;
+        this.genere = genere;
+        this.director = disponibles;
+    }
+}
